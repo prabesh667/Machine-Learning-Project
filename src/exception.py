2ae1f2607c508fe4d8bc228a12f3e3df6c1033cb
prabesh667 <prabeshgupta@hotmail.com>
2023-03-29 14:28:46 +0200

    correction in exception.py for logging update

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,10 @@
 import sys
+from src.logger import logging
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="error occured in python script name [{0}] line number [{1}] error message[{2}]".formart(
+    error_message="error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
@@ -15,6 +16,8 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+
     
 
     
